aa077f8e3ca2f142c2b8a996b642f5cef9325268
Ryan Niswonger <ryan.niswonger@gmail.com>
2015-08-24 11:54:53 -0700

    Add WP editor defaults for image handling

--- lib/extras.php
+++ lib/extras.php
@@ -31,3 +31,34 @@ function excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+/**
+ * Enqueue Styles
+ */
+function enqueue_scripts() {
+  // nothing yet...
+}
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts' );
+
+
+/**
+ * Default media gallery image options
+ */
+function theme_setup() {
+    // Set default values for the upload media box
+  update_option('image_default_align', 'none' );
+  update_option('image_default_link_type', 'none' );
+  update_option('image_default_size', 'large' );
+}
+add_action('after_setup_theme', __NAMESPACE__ . '\\theme_setup');
+
+
+/**
+ * Remove width and height attribute on editor-added images
+ */
+function remove_thumbnail_dimensions( $html ) {
+    $html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
+    return $html;
+}
+add_filter( 'post_thumbnail_html', __NAMESPACE__ . '\\remove_thumbnail_dimensions', 10 );
+add_filter( 'image_send_to_editor', __NAMESPACE__ . '\\remove_thumbnail_dimensions', 10 );
