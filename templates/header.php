de26414d8fea35573125457ad5c77eacce25c134
Ryan Niswonger <ryan.niswonger@gmail.com>
2015-08-24 11:49:15 -0700

    Add optional script for Bootstrap nav and nav walker in header.php template

--- templates/header.php
+++ templates/header.php
@@ -10,3 +10,40 @@
     </nav>
   </div>
 </header>
+
+<?php
+/*
+
+// replace above code with the below for the Bootstrap nav
+// https://gist.github.com/retlehs/1b49f6c00d5140962d56
+
+<?php
+  // https://github.com/twittem/wp-bootstrap-navwalker
+  require_once('wp-bootstrap-navwalker-master/wp_bootstrap_navwalker.php');
+?>
+
+<header class="banner navbar navbar-default navbar-static-top" role="banner">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+    </div>
+
+    <nav class="collapse navbar-collapse" role="navigation">
+      <?php
+      if (has_nav_menu('primary_navigation')) :
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav']);
+      endif;
+      ?>
+    </nav>
+  </div>
+</header>
+
+*/
+
+?>
