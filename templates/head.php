088eb1220a41a4b8b804245c13d810ff7b98fbb7
Ryan Niswonger <ryan.niswonger@gmail.com>
2015-08-24 11:52:12 -0700

    Add scripts to enable HTML5 and media queries in IE8

--- templates/head.php
+++ templates/head.php
@@ -3,4 +3,10 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
 </head>
